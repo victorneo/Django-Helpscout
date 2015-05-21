d78a4604d36c34481fb0fb6f9e26783cd2f0ebae
Jon <jonklo@users.noreply.github.com>
2015-05-21 13:09:14 +0200

    Removed ineligible new test.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -35,10 +35,6 @@ class TestHelpscoutHelper(unittest.TestCase):
         decorator = helpers.helpscout_request(lambda x: True)
         self.assertTrue(decorator(request))
 
-        # Should also work when header is a string
-        request.META = {'HTTP_X_HELPSCOUT_SIGNATURE': str(computed_sig)}
-        self.assertTrue(decorator(request))
-
         # Invalid signature should return a response code of 401
         request.META['HTTP_X_HELPSCOUT_SIGNATURE'] = u'234'
         response = decorator(request)
